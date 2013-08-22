c7016fdb59cfb05850bf9ca41a11bbab4c99bf85
Ludovic Ferrandis <ludovic.ferrandis@intel.com>
2013-08-22 16:07:55 +0200

    [Network Filtering] Add Network Filtering support
    Add 2 new settings:
    1 - netf_enabled (boolean): To activate or deactivate the network filtering
    2 - netf_entries (str list): List of supported network
    
    Add org.freedesktop.DBus.Properties DBUS Interface to
    com.intel.dLeynaRenderer.Manager root object.
    
    Add 4 new methodes to com.intel.dLeynaRenderer.Manager interface
    1 - WhiteListEnable
    2 - WhiteListAddEntries
    3 - WhiteListRemoveEntries
    4 - WhiteListClear
    
    Signed-off-by: Ludovic Ferrandis <ludovic.ferrandis@intel.com>

---
+++ libdleyna/renderer/manager.h
@@ -0,0 +1,55 @@
+/*
+ * dLeyna
+ *
+ * Copyright (C) 2013 Intel Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Ludovic Ferrandis <ludovic.ferrandis@intel.com>
+ *
+ */
+
+#ifndef DLR_MANAGER_H__
+#define DLR_MANAGER_H__
+
+#include <libdleyna/core/connector.h>
+#include <libgupnp/gupnp-context-manager.h>
+
+#include "task.h"
+
+typedef struct dlr_manager_t_ dlr_manager_t;
+typedef void (*dlr_manager_task_complete_t)(dlr_task_t *task, GError *error);
+
+dlr_manager_t *dlr_manager_new(dleyna_connector_id_t connection,
+			       GUPnPContextManager *connection_manager);
+
+void dlr_manager_delete(dlr_manager_t *manager);
+
+void dlr_manager_wl_enable(dlr_task_t *task);
+
+void dlr_manager_wl_add_entries(dlr_task_t *task);
+
+void dlr_manager_wl_remove_entries(dlr_task_t *task);
+
+void dlr_manager_wl_clear(dlr_task_t *task);
+
+void dlr_manager_get_all_props(dlr_manager_t *manager,
+			       dlr_task_t *task,
+			       dlr_manager_task_complete_t cb);
+
+void dlr_manager_get_prop(dlr_manager_t *manager,
+			  dlr_task_t *task,
+			  dlr_manager_task_complete_t cb);
+
+#endif /* DLR_MANAGER_H__ */
